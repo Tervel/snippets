5f33aa567ee7fa4ba85b26b9fdc18356da510e02
Jordan Anslow <jordananslow@gmail.com>
2015-07-16 02:56:21 +1000

    jquery pre-dom loaded example

--- javascript/jquery/js/app.js
+++ javascript/jquery/js/app.js
@@ -2,3 +2,10 @@
 $(".warning").hide();
 //Show Warning Slowly
 $(".warning").show("slow");
+
+// If jquery is loaded first, loading at bottom of page is best practice as we can assume the DOM has been loaded
+// function myCode() {
+//     $(".warning").hide().show("slow");
+// }
+
+// $(document).ready(myCode);
