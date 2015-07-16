2eae73c98dced8bd2e19e7f0171112da92d8bd5b
Jordan Anslow <jordananslow@gmail.com>
2015-07-17 01:16:52 +1000

    jquery progress

---
+++ javascript/jqueryspoiler/js/app.js
@@ -0,0 +1,13 @@
+// Prevent spoilerphobes from seeing spoilers
+// Solution: Hide spoilers and reveal them through user interaction
+
+/**
+ * 1. Hide spoiler
+ * 2. Add a button
+ * 3. When button pressed
+ *    3.1 Show spoiler
+ *    3.2 Get rid of button
+**/
+
+$(".spoiler span").hide();
+$(".spoiler").append("<button>Reveal Spoiler!</button>");
