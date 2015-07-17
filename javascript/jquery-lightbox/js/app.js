f95ee720b8a66e35bc6cac52f816bd702f0d380c
Jordan Anslow <jordananslow@gmail.com>
2015-07-18 03:45:04 +1000

    lightbox complete

--- javascript/jquery-lightbox/js/app.js
+++ javascript/jquery-lightbox/js/app.js
@@ -11,17 +11,39 @@
 // 3. When overlay is clicked
 //  3.1 Hide the overlay
 
-// Add overlay
+
 var $overlay = $('<div id="overlay"></div>'); // jQuery object
 var $image = $("<img>");
+var $caption = $("<p></p>");
 
+// Add image to overlay
 $overlay.append($image); // appended to overlay
+
+// Add caption to overlay
+$overlay.append($caption);
+
+// Add overlay
 $("body").append($overlay) // appended to body
 
 // Capture the click event on a link to an image
 $("#imageGallery a").click(function(event) {
     event.preventDefault();
-    var href = $(this).attr("href");
+    var imageLocation = $(this).attr("href");
+
+    // Update overlay with the image linked in the link
+    $image.attr("src", imageLocation);
+
+    // Show the overlay
     $overlay.show();
-    console.log(href);
+
+    // Get child's alt attribute and set caption
+    var captionText = $(this).children("img").attr("alt");
+    $caption.text(captionText);
 });
+
+// When overlay is clicked
+$overlay.click(function() {
+    $(this).hide();
+});
+
+
