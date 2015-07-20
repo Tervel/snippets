dd2fa837b203185b3ac25e13ff572408e066b4c9
Jordan Anslow <jordananslow@gmail.com>
2015-07-20 15:19:38 +1000

    Playlist syntax correction

--- javascript/playlist/app.js
+++ javascript/playlist/app.js
@@ -7,7 +7,6 @@ var manOfSteel = new Movie("Man of Steel", 2013, "2:23:00");
 
 playlist.add(hereComesTheSun);
 playlist.add(walkingOnSunshine);
-
 playlist.add(manOfSteel);
 
 var playlistElement = document.getElementById("playlist");
@@ -18,16 +17,16 @@ var playButton = document.getElementById("play");
 playButton.onclick = function() {
   playlist.play();
   playlist.renderInElement(playlistElement);
-}
+};
 
 var nextButton = document.getElementById("next");
 nextButton.onclick = function() {
   playlist.next();
   playlist.renderInElement(playlistElement);
-}
+};
 
 var stopButton = document.getElementById("stop");
 stopButton.onclick = function() {
   playlist.stop();
   playlist.renderInElement(playlistElement);
-}
+};
