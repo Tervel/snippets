6d7877b11655269ece90b650a228274161dc0bc1
Jordan Anslow <jordananslow@gmail.com>
2015-07-23 04:08:51 +1000

    jquery sticky plugin example

--- javascript/jquery-plugin-sticky/js/main.js
+++ javascript/jquery-plugin-sticky/js/main.js
@@ -6,3 +6,32 @@ $(".animsition").animsition({
   outDuration: 500
 });
 
+$(".header").sticky({
+    getWidthFrom: '.container',
+    responsiveWidth: true
+});
+
+$(".header").on('sticky-start', function() {
+    $('.description').html('We build <strong>great</strong> apps');
+});
+
+$(".header").on('sticky-end', function() {
+    $('.description').html('We build apps');
+});
+
+$(".work").sticky({
+    topSpacing: 60,
+    getWidthFrom: '.container',
+    responsiveWidth: true
+});
+
+$(".work").on('sticky-start', function() {
+    $(".work").append(' <a href="mailto:foo@foo.com" class="email-text">Email&nbspus</a>');
+});
+
+$(".work").on('sticky-end', function() {
+    $(".email-text").remove();
+});
+
+
+
